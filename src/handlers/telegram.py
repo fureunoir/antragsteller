5ad57b7588f453d44f8ae132f869c0f91dd42847
Egor fureunoir Gorbunov <contact@fureunoir.com>
2024-10-17 05:50:01 +0300

    Initial Commit

--- src/handlers/telegram.py
+++ src/handlers/telegram.py
@@ -6,8 +6,9 @@ from aiogram.types import Update
 from src.handlers import router
 from src.helpers.bot import handle_message
 
+url_part = getenv("TELEGRAM_BOT_TOKEN")
 
-@router.post(f"/webhook/{getenv("TELEGRAM_BOT_TOKEN")}", include_in_schema=False)
+@router.post(f"/webhook/{url_part}", include_in_schema=False)
 async def telegram_webhook(update: Update):
     create_task(handle_message(update))
     return {"status": "ok"}
